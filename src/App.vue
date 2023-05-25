154c53320a023a72fa30e452df393892a1a8b5e1
luisalberto <luisalberto@sefaz.pi.gov.br>
2023-05-25 08:21:47 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<script setup>
+import Item from './components/Item.vue'
+</script>
+
+<script>
+import Dados from './Dados/Services.json'
+export default {
+  data() {
+    return{
+      servicos: null
+    }
+  },
+  mounted() {
+    this.servicos = Dados;
+    console.log(Dados);
+  }
+}
+</script>
+
+<template>    
+    <img src="./assets/tesoura.png" class="logo" alt="Barbearia"/> 
+    <h1 style="font-size: large;">Seja bem vindo(a)!</h1>
+  <hr /> 
+  <div class="items">
+    <Item
+    v-for="servico, index in servicos"
+    :key="index"
+    :item="servico"
+    />
+  </div>
+</template>
+
+<style scoped>
+html{
+  background: url("./assets/barbearia.jpg");
+  background-repeat: no-repeat;
+}
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+.items{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
---
+++ src/components/Item.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="card">
+     <a href="item.href">
+        <img :src="item.img" alt="Icone do servico"/>
+        <span>{{ item.titulo }}</span>
+     </a>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['item']
+}
+</script>
+
+<style scoped>
+div{
+    margin: 20px 30px 20px 30px;
+    display: block;
+    width: 100px;
+}
+img{
+    width: 80px;    
+}
+span{
+    font-weight: 900;
+    justify-content: center;
+}
+.card{
+    background-color: rgba( 255, 255, 255, .5);
+    display: flex ;
+    justify-content: center;
+    flex-direction: column;
+    min-width: 80px;
+    min-height: 150px;
+    border: 1px solid grey;
+    border-radius: 5%;
+    opacity: .9;
+    box-shadow: 0 0 2em grey;
+    transition: box-shadow .5s;
+}
+</style>
